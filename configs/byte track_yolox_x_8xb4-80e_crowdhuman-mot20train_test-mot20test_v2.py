f1a6ef5b1c6f75585cefd55a867c3ea269b25e5e
najabba <93382275+najabba@users.noreply.github.com>
2024-03-06 23:07:27 +0100

    Create byte track_yolox_x_8xb4-80e_crowdhuman-mot20train_test-mot20test_v2.py
    Test new metrics

---
+++ configs/byte track_yolox_x_8xb4-80e_crowdhuman-mot20train_test-mot20test_v2.py
@@ -0,0 +1,9 @@
+__base__ = ['bytetrack_yolox_x_8xb4-80e_crowdhuman-mot20train_test-mot20test.py']
+
+test_evaluator = dict(
+    type='NAJMetrics',
+    postprocess_tracklet_cfg=[
+        dict(type='InterpolateTracklets', min_num_frames=5, max_num_frames=20)
+    ],
+    format_only=True,
+    outfile_prefix='./mot_20_test_res')
