60892d6746901906b51b3953ece4b517cb27cfd4
najabba <93382275+najabba@users.noreply.github.com>
2024-03-12 15:21:13 +0100

    Update faster-rcnn_r50_fpn_1x_mot20.py
    Changed base files

--- configs/faster_rcnn/faster-rcnn_r50_fpn_1x_mot20.py
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_1x_mot20.py
@@ -1,7 +1,3 @@
-_base_ = [
-    '../_base_/models/faster-rcnn_r50_fpn.py',
-    '../_base_/datasets/mot_challenge_det.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
-]
+_base_ = ['./faster-rcnn_r50_fpn_1x_mot17.py']
 
 data_root = 'data/MOT20/'
