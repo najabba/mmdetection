1f58bc26573374f7214935a0a074ffc273c2d271
najabba <93382275+najabba@users.noreply.github.com>
2024-03-12 15:15:36 +0100

    Create faster-rcnn_r50_fpn_1x_mot20.py

---
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_1x_mot20.py
@@ -0,0 +1,7 @@
+_base_ = [
+    '../_base_/models/faster-rcnn_r50_fpn.py',
+    '../_base_/datasets/mot_challenge_det.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+data_root = 'data/MOT20/'
