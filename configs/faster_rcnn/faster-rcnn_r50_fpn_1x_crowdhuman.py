db087fc540878064b4faa3ef174d7cdb95d697f1
najabba <93382275+najabba@users.noreply.github.com>
2024-03-17 12:57:47 +0100

    Update faster-rcnn_r50_fpn_1x_crowdhuman.py
    Typo _base_

--- configs/faster_rcnn/faster-rcnn_r50_fpn_1x_crowdhuman.py
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_1x_crowdhuman.py
@@ -1,4 +1,4 @@
-base_ = [
+_base_ = [
     '../_base_/models/faster-rcnn_r50_fpn.py',
     '../_base_/datasets/crowdhuman.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
